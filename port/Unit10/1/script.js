77275d63e9d5c997e91e43c09ae509791ea8c969
Aashruti11 <163490837+Aashruti11@users.noreply.github.com>
2024-04-12 15:09:47 +0545

    Add files via upload

---
+++ port/Unit10/1/script.js
@@ -0,0 +1,9 @@
+var int = 10;
+var float = 12.9;
+const boolean = 8>20;
+var string = "Bibash Bhatta";
+
+document.write(int,"<br>");
+document.write(float,"<br>");
+document.write(boolean,"<br>");
+document.write(string,"<br>");
